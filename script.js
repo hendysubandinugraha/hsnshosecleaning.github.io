fcdc8fa17b8c69985b70d41c7810562b3d8a2e9c
hendysubandinugraha <hendysubandi277@gmail.com>
2023-01-25 08:14:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Toggle class active
+
+const navbarnav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu di klik
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarnav.classList.toggle('active');
+};
+
+
+// KLik diluar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+});
